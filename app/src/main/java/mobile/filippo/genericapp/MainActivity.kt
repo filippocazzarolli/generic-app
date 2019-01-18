63f3bc44da82e8e2ab6020dc33afd55a05766020
filippocazzarolli@gmail.com <filippo123>
2019-01-18 17:22:22 +0100

    work profile activity

--- app/src/main/java/mobile/filippo/genericapp/MainActivity.kt
+++ app/src/main/java/mobile/filippo/genericapp/MainActivity.kt
@@ -2,10 +2,11 @@ package mobile.filippo.genericapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.transition.TransitionInflater
 import android.view.Window
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-//        initAnimation()
+        initAnimation()
         initButton()
 
         window.allowEnterTransitionOverlap = false
@@ -27,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         btnProfile.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
 
     }
@@ -39,7 +40,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initAnimation() {
-        val enterTransition = TransitionInflater.from(this).inflateTransition(R.transition.explode)
-        window.enterTransition = enterTransition
+//        val enterTransition = TransitionInflater.from(this).inflateTransition(R.transition.explode)
+//        window.enterTransition = enterTransition
+
+
+        val myAnim1 = AnimationUtils.loadAnimation(this, R.anim.slide_in_up)
+        val myAnim2 = AnimationUtils.loadAnimation(this, R.anim.slide_in_up)
+        val myAnim3 = AnimationUtils.loadAnimation(this, R.anim.slide_in_up)
+        val myAnim4 = AnimationUtils.loadAnimation(this, R.anim.slide_in_up)
+        btnProfile.startAnimation(myAnim1)
+        myAnim2.startOffset = 200
+        btnCard.startAnimation(myAnim2)
+        myAnim3.startOffset = 400
+        btnContact.startAnimation(myAnim3)
+        myAnim4.startOffset = 600
+        btnNews.startAnimation(myAnim4)
     }
 }
